173dc64426b061506c8841b644d6c0bc04453399
lisettereins <lreins@tlu.ee>
2025-09-15 20:22:11 +0300

    feat: extend counter with multiple increment/decrement steps

--- 2025-09-11/my-app/src/components/Counter.tsx
+++ 2025-09-11/my-app/src/components/Counter.tsx
@@ -5,17 +5,19 @@ import "../App.css"
 function Counter() {
   const [count, setCount] = useState(0)
 
-  function increaseCounter() {
-    setCount(count => count + 1)
-  }
-
   return (
     <>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={increaseCounter}> 
-          count is {count}
-        </button>
+        <button onClick={() => setCount(count + 100)}>+100</button>
+        <button onClick={() => setCount(count + 50)}>+50</button>
+        <button onClick={() => setCount(count + 25)}>+25</button>
+        <button onClick={() => setCount(count + 1)}>+1</button>
+        <button>count is: {count}</button>
+        <button onClick={() => setCount(count - 1)}>-1</button>
+        <button onClick={() => setCount(count - 25)}>-25</button>
+        <button onClick={() => setCount(count - 50)}>-50</button>
+        <button onClick={() => setCount(count - 100)}>-100</button>
       </div>
     </>
   )
