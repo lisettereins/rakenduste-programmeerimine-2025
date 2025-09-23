9e13ee13b7aa279be29842d2db3c732976cec041
lisettereins <lreins@tlu.ee>
2025-09-23 23:26:33 +0300

    feat: added mui box to about page

--- 2025-09-18/2025-09-18/src/components/About.tsx
+++ 2025-09-18/2025-09-18/src/components/About.tsx
@@ -1,19 +1,24 @@
 import "../About.css";
-import { Button, TextField } from "@mui/material";
+import {
+  BottomNavigation,
+  BottomNavigationAction,
+  Box,
+  Button,
+  TextField,
+} from "@mui/material";
 import { Link, Outlet } from "react-router";
 import useLocalStorage from "../useLocalStorage";
 import { useRef } from "react";
 
 function About() {
-
-  const [email, setEmail] = useLocalStorage<string>('email', '')
+  const [email, setEmail] = useLocalStorage<string>("email", "");
   const emailInputRef = useRef<HTMLInputElement>(null);
 
-   const handleClick = () => {
+  const handleClick = () => {
     const inputValue = emailInputRef.current?.value;
     if (inputValue !== undefined) {
       setEmail(inputValue);
-      console.log('Saved email:', inputValue);
+      console.log("Saved email:", inputValue);
     }
   };
 
@@ -21,40 +26,50 @@ function About() {
     <>
       <div>About</div>
       <div id="container">
-        <div className="aboutContainer"><h1>Lisette Reins</h1>
-        <h3>List huvidest/hobidest:</h3>
-        <ul>
-          <li>Matkamine</li>
-          <li>Koerte koolitus ja hingeelu</li>
-          <li>Programmeerimine</li>
-        </ul></div>
+        <div className="aboutContainer">
+          <h1>Lisette Reins</h1>
+          <h3>List huvidest/hobidest:</h3>
+          <ul>
+            <li>Matkamine</li>
+            <li>Koerte koolitus ja hingeelu</li>
+            <li>Programmeerimine</li>
+          </ul>
+        </div>
         <br />
-       <div className="contactinfoContainer"> 
-        <TextField
-          label="E-mail"
-          id="filled-hidden-label-small"
-          defaultValue={email}
-          variant="filled"
-          size="small"
-          inputRef={emailInputRef}
-        />
-        <br />
-        <br />
-        <TextField
-          id="outlined-multiline-flexible"
-          label="Tekstiväli"
-          multiline
-          maxRows={4}
-        />
-        <br />
-        <br />
-        <Button variant="contained" onClick={handleClick}>Saada</Button></div>
+        <Box component="section" sx={{ p: 2, border: "1px dashed grey" }}>
+          <div className="contactinfoContainer">
+            <TextField
+              label="E-mail"
+              id="filled-hidden-label-small"
+              defaultValue={email}
+              variant="filled"
+              size="small"
+              inputRef={emailInputRef}
+            />
+            <br />
+            <br />
+            <TextField
+              id="outlined-multiline-flexible"
+              label="Tekstiväli"
+              multiline
+              maxRows={4}
+            />
+            <br />
+            <br />
+            <Button variant="contained" onClick={handleClick}>
+              Saada
+            </Button>
+          </div>
+        </Box>
       </div>
-      <nav>
-        <Link to="/">Home</Link>
-        <Link to="/something">Something</Link>
-      </nav>
+
       <Outlet />
+      <BottomNavigation>
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="/something">Something</Link>
+        </nav>
+      </BottomNavigation>
     </>
   );
 }
