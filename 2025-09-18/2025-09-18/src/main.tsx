d03e975f2c6cd06047537722e6c3da49cdd823c1
lisettereins <lreins@tlu.ee>
2025-09-23 19:27:18 +0300

    feat: added createTheme and ThemeProvider to main.tsx

--- 2025-09-18/2025-09-18/src/main.tsx
+++ 2025-09-18/2025-09-18/src/main.tsx
@@ -1,15 +1,25 @@
-
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import { createRoot } from "react-dom/client";
+import "./index.css";
 //import App from './App.tsx'
-import { createHashRouter/* , HashRouter */ } from 'react-router'
-import Home from './components/Home.tsx';
-import Something from './components/Something.tsx';
-import About from './components/About.tsx';
-import { RouterProvider } from 'react-router';
-import { StrictMode } from 'react';
+import { createHashRouter /* , HashRouter */ } from "react-router";
+import Home from "./components/Home.tsx";
+import Something from "./components/Something.tsx";
+import About from "./components/About.tsx";
+import { RouterProvider } from "react-router";
+import { StrictMode } from "react";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { pink, purple } from "@mui/material/colors";
 
-
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: pink[500],
+    },
+    secondary: {
+      main: purple[500],
+    },
+  },
+});
 const router = createHashRouter([
   {
     path: "/",
@@ -24,10 +34,12 @@ const router = createHashRouter([
     element: <Something />,
   },
 ]);
-createRoot(document.getElementById('root')!).render(
-   <>
-   <StrictMode>
-    <RouterProvider router={router} />
-   </StrictMode>
-   </>
-)
+createRoot(document.getElementById("root")!).render(
+  <>
+    <ThemeProvider theme={theme}>
+      <StrictMode>
+        <RouterProvider router={router} />
+      </StrictMode>
+    </ThemeProvider>
+  </>
+);
