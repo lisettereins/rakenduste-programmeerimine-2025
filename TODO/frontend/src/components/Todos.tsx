35f8984abaa0d0c6970bdbbd23f4ae9629675f89
lisettereins <lreins@tlu.ee>
2025-09-29 20:22:33 +0300

    feat: added a delete function and button to Todos component

--- TODO/frontend/src/components/Todos.tsx
+++ TODO/frontend/src/components/Todos.tsx
@@ -1,4 +1,4 @@
-import { Box, List, ListItem, Typography } from "@mui/material";
+import { Box, Button, List, ListItem, Typography } from "@mui/material";
 import { useEffect, useState } from "react";
 
 type Todo = {
@@ -23,26 +23,68 @@ const Todos = () => {
     fetchTodos();
   }, []);
 
+ 
+
+  const deleteTodo = async (id: string) => {
+    try {
+      const response = await fetch("http://localhost:3000/todos", {
+        method: "DELETE",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ id }),
+      });
+
+      if (response.ok) {
+        console.log("Success", response);
+        fetchTodos();
+        
+        // Snackbar success
+      } else {
+        console.warn("No success");
+        // Snackbar
+      }
+    } catch (error) {
+      console.warn(error);
+    }
+  };
+
   return (
     <Box>
       <Typography variant="h1">To do</Typography>
-      <TodoList todos={todos} />
+      <TodoList todos={todos} onDelete={deleteTodo} />
     </Box>
   );
 };
 
 type TodoListProps = {
-    todos: Todo[];
+  todos: Todo[];
+  onDelete: (id: string) => void;
 };
 
-const TodoList: React.FC<TodoListProps> = ({todos}) => {
-    return (
-        <List>
-            {todos.map((todo) => (
-                <ListItem key={todo.id}>{JSON.stringify(todo)}</ListItem>
-            ))}
-        </List>
-    );
+const TodoList: React.FC<TodoListProps> = ({ todos, onDelete }) => {
+  return (
+    <List>
+      {todos.map((todo) => (
+        <ListItem
+          key={todo.id}
+          secondaryAction={
+            <Button
+              variant="contained"
+              color="error"
+              onClick={() => onDelete(todo.id)}
+            >
+              Delete
+            </Button>
+          }
+        >
+          {JSON.stringify(todo)}{" "}
+        </ListItem>
+      ))}
+      <Button variant="contained" color="success" type="submit"></Button>
+    </List>
+  );
 };
 
 export default Todos;
