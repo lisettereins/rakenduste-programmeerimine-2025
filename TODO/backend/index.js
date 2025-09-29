579ef6b0d57b730d23d5d64caeb45859facd9841
lisettereins <lreins@tlu.ee>
2025-09-29 17:14:33 +0300

    feat: added todo route to index.js

--- TODO/backend/index.js
+++ TODO/backend/index.js
@@ -2,9 +2,12 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const cors = require("cors");
+const todoRoutes = require("./routes/todo.routes");
 
 app.use(cors());
 app.use(express.json());
+
+app.use("/todos", todoRoutes)
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
